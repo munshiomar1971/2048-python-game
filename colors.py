cef2321fe7a351ad1bdc510a0ee8fbdf54c2201b
worstfrontendcoder <omarsumon1030@gmail.com>
2021-08-05 12:15:49 -0500

    inital commit

---
+++ colors.py
@@ -0,0 +1,52 @@
+# colors
+
+GRID_COLOR = "#a39489"
+EMPTY_CELLCOLOR = "#c2b3a9"
+SCORE_label_FONT = ("Verdana", 24)
+SCORE_font = ("Helvetica", 36, "bold")
+LOSE_FONT = ("Helvetica", 48, "bold") # game over font 
+LOSE_FONT_COLOR = "#ffffff"
+winner_bg = "#ffcc00"
+LOSER_bg = "a39489"
+
+CELL_COLORS = {
+	2: "#fcefe6",
+	4: "#f2e8cb",
+	8: "#f5b682",
+	16: "#f29446",
+	32: "#f29446",
+	64: "#eb64c2e",
+	128: "#ede291",
+	256: "#fce130",
+	512: "#ffdb4a",
+	1024: "#f0b922",
+	2048: "#fad74d"
+}
+
+CELL_NUM_COLORS = {
+	2: "#695c57",
+	4: "#695c57",
+	8: "#ffffff",
+	16: "#ffffff",
+	32: "#ffffff",
+	64: "#ffffff",
+	128: "#ffffff",
+	256: "#ffffff",
+	512: "#ffffff",
+	1024: "#ffffff",
+	2048: "#ffffff"
+}
+
+CELL_NUM_FONTS = {
+	2:    ("Helvetica", 55, "bold"),
+	4:    ("Helvetica", 55, "bold"),
+	8:    ("Helvetica", 55, "bold"),
+	16:   ("Helvetica", 55, "bold"),
+	32:   ("Helvetica", 55, "bold"),
+	64:   ("Helvetica", 55, "bold"),
+	128:  ("Helvetica", 55, "bold"),
+	256:  ("Helvetica", 55, "bold"),
+	512:  ("Helvetica", 55, "bold"),
+	1024: ("Helvetica", 55, "bold"),
+	2048: ("Helvetica", 55, "bold")
+}
